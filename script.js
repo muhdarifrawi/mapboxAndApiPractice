f0a1ec0e58f205368e85d7c713e9e3bf1ebd57f5
Ubuntu <ubuntu@ip-172-31-87-186.ec2.internal>
2019-09-25 09:20:10 +0000

    used array of objects instead to indicate multiple places.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 /* global mapboxgl*/
 
+let places = [
+    {
+        "name":"Singapore Bird Park",
+        "position":[103.7064, 1.3187]
+    },
+    {
+        "name":"Singapore Zoo",
+        "position":[103.7930, 1.4043]
+    },
+    {
+        "name":"Changi Airport",
+        "position":[103.9915, 1.3644]
+    }
+];
+
 mapboxgl.accessToken = 'pk.eyJ1IjoibXVoZGFyaWZyYXdpIiwiYSI6ImNrMHl3dDNycTBpdDkzaHJ4cHJtdnU0YXoifQ.8ceqA9s8SqMSbVa1rsZgvg';
 
 var map = new mapboxgl.Map({
@@ -9,9 +24,24 @@ var map = new mapboxgl.Map({
     zoom: 10.5
 });
 
-var marker = new mapboxgl.Marker()
-    .setLngLat([103.8198, 1.3521])
-    .setPopup(new mapboxgl.Popup({offset:25})//creates popup. Do note that it shows itself when you click on the marker.
-    .setHTML("<h3>Singapore</h3><p>Half Summer, Half Monsoon.</p>"))//inserts HTML directly from script.js
+//We're keeping these marker just in case.
+// var marker = new mapboxgl.Marker()
+//     .setLngLat([103.8198, 1.3521])
+//     .setPopup(new mapboxgl.Popup({offset:25})//creates popup. Do note that it shows itself when you click on the marker.
+//     .setHTML("<h3>Singapore</h3><p>Half Summer, Half Monsoon.</p>"))//inserts HTML directly from script.js
+//     .addTo(map);
+
+let markers = [];
+
+for (let p of places){
+    let m = new mapboxgl.Marker()
+    .setLngLat(p.position)
+    .setPopup(new mapboxgl.Popup({offset:25})
+    .setHTML("<p>${p.name}</p>"))
     .addTo(map);
+    
+    markers.push(m);
+};
+
+
     
