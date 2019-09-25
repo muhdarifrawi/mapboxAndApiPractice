1ce5f8b09c0c7bb0f1d59385358a6c0301ac1117
Ubuntu <ubuntu@ip-172-31-87-186.ec2.internal>
2019-09-25 07:40:43 +0000

    Shifted mapbox script to script.js.changed center in mapboxgl to Singapore.

---
+++ script.js
@@ -0,0 +1,11 @@
+/* global mapboxgl*/
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibXVoZGFyaWZyYXdpIiwiYSI6ImNrMHl3dDNycTBpdDkzaHJ4cHJtdnU0YXoifQ.8ceqA9s8SqMSbVa1rsZgvg';
+
+        
+var map = new mapboxgl.Map({
+    container: 'map', //don't need to include # to target the ID for this case. 
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center:[103.8198, 1.3521], //starting position as [lng, lat]
+    zoom: 10.5
+});
